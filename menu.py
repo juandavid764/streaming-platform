e2b4f8a3f5f40d12c5345faba489cb9a35d9db7b
Sebastian <semega1206@gmail.com>
2024-09-04 16:01:40 -0500

    le meti

---
+++ menu.py
@@ -0,0 +1,24 @@
+import json
+
+
+# importamos las operaciones del crud que ya definimos
+from crud_operations import create_record, read_record, update_record, delete_record, get_all_records, publishers_with_high_sales
+
+
+def convert_to_json(collection):
+    return json.dumps(collection, indent=4)
+
+
+def menu():
+    while True:
+        print("""---------------------------------------
+Select wanted option:
+1. Create a new document
+2. read a document
+3. Updated a document
+4. Delete a document
+5. get all documents
+6. get publishers with more than 500000TotalSales
+7. get out
+---------------------------------------""")
+        option1 = input("option:  ")
