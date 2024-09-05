26144957ed8be67aad3beac71b76acda02432da0
juandavid764 <juandaviderazo2401@gmail.com>
2024-09-05 17:46:12 -0500

    estructuraFinalArchivos

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import json  # Importing json for
 
 
 # importamos las operaciones del crud que ya definimos
-from crud_operations import useQuerys, create_record, read_record, update_record, delete_record, get_all_records, publishers_with_high_sales
+from crud_operations import useQuerys, create_record, read_document, update_record, delete_record, get_all_records, publishers_with_high_sales
 
 
 def convert_to_json(collection):
--- menu.py
+++ menu.py
@@ -2,7 +2,7 @@ import json
 
 
 # importamos las operaciones del crud que ya definimos
-from crud_operations import create_record, read_record, update_record, delete_record, get_all_records, publishers_with_high_sales
+from crud_operations import create_record, read_document, update_record, delete_record, get_all_records, publishers_with_high_sales
 
 
 def convert_to_json(collection):
@@ -22,3 +22,40 @@ Select wanted option:
 7. get out
 ---------------------------------------""")
         option1 = input("option:  ")
+
+
+
+def readCollection():
+    
+    print("""Select collection:
+    1. Publisher
+    2. User
+    3. Videogame
+    4. Review""")
+    optionSelected = input("Option: ")
+    
+    if optionSelected == "1":
+        collectionName = "publisher"
+        numbreId = input("Id: ")
+        idCollection = collectionName+numbreId
+
+        
+        read_document(collectionName, idCollection)
+
+    elif optionSelected == "2":
+        collectionName = "user"
+        numbreId = input("Id: ")
+        idCollection = collectionName+numbreId
+
+    elif optionSelected == "3":
+        collectionName = "videogame"
+        numbreId = input("Id: ")
+        idCollection = collectionName+numbreId
+    elif optionSelected == "4":
+        collectionName = "review"
+        numbreId = input("Id: ")
+        idCollection = collectionName+numbreId
+    else:
+        print("Invalid option. Please try again.")
+    
+    return idCollection
